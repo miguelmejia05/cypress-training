d6cbb4eb6151c2a997a0bf39eb03403bf91c784a
Miguel Mejia Pabon <miguel.mejiap@perficient.com>
2023-02-07 17:07:24 -0500

    Added new test case to buy a t-shirt using css selectors

---
+++ cypress/e2e/buy-shirt.cy.ts
@@ -0,0 +1,12 @@
+describe('Buy a t-shirt', () => {
+	it('then the t-shirt should be bought', () => {
+		cy.visit('https://www.amazon.com/');
+		cy.get('div[class*=\'search-field\'] input').type('t-shirt');
+		cy.get('div[class*=\'search-submit\'] input').click();
+		cy.get('span[data-component-type*=\'search-results\'] div[data-component-type*=\'search-result\']:nth-child(2)')
+			.click();
+		cy.get('input#add-to-cart-button').click();
+		cy.get('input[name*=\'Checkout\']:first-child').click();
+		cy.get('h1').should('contain.text', 'Sign in');
+	});
+});
